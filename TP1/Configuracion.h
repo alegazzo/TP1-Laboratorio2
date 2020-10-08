132d188cc115fea102e6decf6507ba9afa0d0851
JuaniBrunero <juanibrunero@gmail.com>
2020-10-08 13:13:18 -0300

    A

--- TP1/Configuracion.h
+++ TP1/Configuracion.h
@@ -5,6 +5,7 @@ void integrantesDelGrupo();
 void realizarCopia();
 void restaurarCopia();
 int generarPin( int );
+
 int cantidad_usuarios_bkp();
 int cantidad_entrenos_bkp();
 #endif // CONFIGURACION_H_INCLUDED
